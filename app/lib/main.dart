4ea0fc889fdef166194dbc7cb09aa965a57a3c00
hiroaki Iwai <hiro_lily_da@hotmail.com>
2018-04-07 23:37:32 +0900

    リストビュー実装

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -9,15 +9,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return new MaterialApp(
       title: 'Welcome to Flutter',
-      home: new Scaffold(
-        appBar: new AppBar(
-          title: new Text('Welcome to Flutter'),
-        ),
-        body: new Center(
-          //child: new Text('Hello World'), // Replace the highlighted text...
-          child: new RandomWords(),
-        ),
-      ),
+      home: new RandomWords()
     );
   }
 }
